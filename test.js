7f912eabc9c269ba229ac9f039a0e299f666112c
Moses Dumbuya <moses2004112@hotmail.com>
2023-05-10 13:30:30 +0200

    Deleted test file. Now included in the Running Examples.

--- test.js
+++
@@ -1,7 +0,0 @@
-
-const { ClientBuilder } = require('@iota/client')
-const client = new ClientBuilder()
-    .node('https://api.lb-0.h.chrysalis-devnet.iota.cafe:443')
-    .localPow(True)
-    .build()
-client.getInfo().then(console.log).catch(console.error)
